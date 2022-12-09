9dea8c75546dcdd7e37e21a9a08f21508ffba71f
FutureRises <futurerises@gmail.com>
2022-12-09 00:54:11 -0500

    final 12/8/2022

--- DroneProject.py
+++ DroneProject.py
@@ -1,5 +1,45 @@
+# written by James Johnson for FutureRises Analytics
+
 from imageai.Detection import ObjectDetection
+from imageai.Detection import VideoObjectDetection
+import djitellopy
 
-recognizer = ObjectDetection()
+def forFrame(frame_number, output_array, output_count):
+    print("For Frame ", frame_number)
+    print("Output for each object : ", output_array)
+    print("Output count for unique objects : ", output_count)
+    print("---------------------End of Frame-------------------")
 
+# instantiating the classes from ImageAI
+PicRecognizer = ObjectDetection()
+VidRecognizer = VideoObjectDetection()
+
+# define the paths
 path_model = "./Models/yolo-tiny.h5"
+path_input = "./Input/attachment.jpg"
+path_output = "./Output/newimage.jpg"
+VidPath_output = "./Output/newVideo"
+VidPath_input = "./Input/Fishermans.mp4"
+
+# using TinyYOLOv3
+PicRecognizer.setModelTypeAsTinyYOLOv3()
+VidRecognizer.setModelTypeAsTinyYOLOv3()
+
+# setting the path the pre-trained model then load
+PicRecognizer.setModelPath(path_model)
+PicRecognizer.loadModel()
+VidRecognizer.setModelPath(path_model)
+PicRecognizer.loadModel()
+
+# detect objects from image
+recognition = PicRecognizer.detectObjectsFromImage(input_image=path_input, output_image_path=path_output)
+VidRecognition = VidRecognizer.detectObjectsFromVideo(input_file_path=VidPath_input,
+                                                      output_file_path=VidPath_output, frames_per_second=29.97,
+                                                      minimum_percentage_probability=30)
+
+# iterate through the items found in image
+for eachItem in recognition:
+    print(eachItem["name"], ":", eachItem["percentage_probability"])
+
+print("And Now the Video: ")
+print(VidRecognition)
